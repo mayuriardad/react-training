35134fc058f72dd544b11f97bd96b303c2542773
mayuriardad <mayuri.ardad@joshsoftware.com>
2020-08-18 10:50:12 +0530

    redux router setup

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 // import App from './App';
-import * as serviceWorker from './serviceWorker';
-// import { Example } from './components/Sample';
-// import UseReducerExample from './components/Sample/ReducerExample';
-import LoginContainer from './containers/LoginContainer';
-  
+import * as serviceWorker from "./serviceWorker";
+
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import Routes from "./routes/Routes";
+import loginDetailsReducer from "./reducers/loginDetailsReducer";
+import projectsReducer from "./reducers/projectsReducer";
+
+const store = createStore(
+  combineReducers({ loginDetailsReducer, projectsReducer })
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <LoginContainer />
-    {/* <Example /> */}
-    {/* <UseReducerExample /> */}
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
